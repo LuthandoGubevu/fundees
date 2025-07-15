7f61a381db61faef9336c2ddcf3e1723e23e07b9
Lutando Gubevu <TidoTeez@gmail.com>
2025-07-15 20:21:23 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBtbdw52IZ4sji35udBsomaoboI0rEzwTc",
+  authDomain: "fundees.firebaseapp.com",
+  projectId: "fundees",
+  storageBucket: "fundees.appspot.com",
+  messagingSenderId: "233053582894",
+  appId: "1:233053582894:web:af7e815ccc3caacc50d1bf"
 };
 
 // Initialize Firebase
